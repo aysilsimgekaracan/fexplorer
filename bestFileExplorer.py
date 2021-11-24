75f746f076788f0eb953ebc01eb461d993e2f0f1
Ayşıl Simge Karacan <aysilsimge@gmail.com>
2021-11-24 17:27:19 +0300

    pick library basic code

---
+++ bestFileExplorer.py
@@ -0,0 +1,7 @@
+from pick import pick
+
+title = 'Please choose your favorite programming language: '
+options = ['Java', 'JavaScript', 'Python', 'PHP', 'C++', 'Erlang', 'Haskell']
+option, index = pick(options, title)
+print(option)
+print(index)
