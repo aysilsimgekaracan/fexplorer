9a065de71577da598e53d378f386a546ff820162
Ayşıl Simge Karacan <aysilsimge@gmail.com>
2021-12-28 17:34:42 +0300

    .

--- files.py
+++ files.py
@@ -1,14 +1,15 @@
 from os import listdir, getcwd
-from os.path import isfile, join, splitext, isdir
+from os.path import splitext, isdir, expanduser
+from pathlib import Path
 
 def isDirectory(path): # Check if a given file path is a file or a directory
 	if isdir(path):
-	    return True
+		return True
 	else:
 		return False
 
 def fileLabel(file, extension):
-	# icon = ""
+    # icon = ""
 	# if extension == "folder":
 	# 	icon = u"\U0001F4C1"
 	# elif extension == ".png":
@@ -19,15 +20,15 @@ def fileLabel(file, extension):
 	# 	icon = u"\U0001F5BA"
 	# else:
 	# 	icon = u"\u003F"
+ 
+    label = f"{file}"
+    return label
 
-	label = f"{file}"
-	return label
-
-
+# find all files and folders in a given path
 def getAllFilesInGivenDirectory(path):
 	# files = [f for f in listdir(path) if isfile(join(path, f))]
 	dirs = listdir(path)
-
+	
 	files =[]
 	for file in dirs:
 		filePath = path + "/" + file
@@ -35,20 +36,32 @@ def getAllFilesInGivenDirectory(path):
 		if extension == "" and isDirectory(filePath) : extension = "folder"
 		files.append({"name": file, "label": fileLabel(file, extension), "path": filePath, "type": extension})
 
-
+	# Add Parent Directory
+	parentPath = getParentDirectory(path)
+	files.append({"name": "Parent Directory", "label": "[Back]", "path": parentPath, "type": "folder"})
 	return files
 
+# Go one directory back
+def getParentDirectory(givenPath): 
+    p = Path(givenPath)
+    return str(p.parent)
 
 
 def getCurrentDirectory():
-	currentDirectory = getcwd()
-	return currentDirectory
+    currentDirectory = getcwd()
+    return currentDirectory
+
+def getHomeDirectory(): # Users home address
+    return expanduser('~')
 
 def main():
-	#print(f"Your current directory: {getCurrentDirectory()}")
+    	#print(f"Your current directory: {getCurrentDirectory()}")
 	path = "/Users/aysilsimge/School/5. Dönem"
-	listFiles(getAllFilesInGivenDirectory(path))
+	print(getAllFilesInGivenDirectory(path))
+	# print(getParentDirectory(path))
 
+# def goBackDirectory(path):
+#     return dirname(path)
 
 if __name__ == "__main__":
     main()
