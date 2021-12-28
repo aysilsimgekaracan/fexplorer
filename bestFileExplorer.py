8069f7620eefa6732b24d8dcc185fcd428af8434
Ayşıl Simge Karacan <aysilsimge@gmail.com>
2021-12-28 13:46:45 +0300

    tried to use register_custom_handler

--- bestFileExplorer.py
+++ bestFileExplorer.py
@@ -2,17 +2,23 @@ from pick import Picker
 import curses
 import files as fl
 
-
-
-def get_label(option): 
+def get_label(option):
 	return option.get("label")
+
+def returnHome(picker): # Return to home directory
+    # path = fl.getHomeDirectory()
+    path = "/Users/aysilsimge/School/5. Dönem"
+    cli(path)
 
 def cli(path):
 	options = fl.getAllFilesInGivenDirectory(path)
 	title = f'Files in location: {path}'
 	picker = Picker(options, title, indicator="=>", options_map_func=get_label)
+	picker.register_custom_handler(ord('h'), returnHome)
+	# picker.register_custom_handler(ord("b"), returnBack)
+ 
 	option, index = picker.start()
-
+	
 	if fl.isDirectory(option.get("path")):
 		cli(option.get("path"))
 
@@ -20,9 +26,17 @@ def cli(path):
 
 
 def main():
+#	path = fl.getCurrentDirectory() # Start with the directory that you are currently in
 	path = "/Users/aysilsimge/School/5. Dönem"
 	cli(path)
 
 
 if __name__ == "__main__":
 	main()
+
+
+# def returnBack(picker):
+#     path = fl.getCurrentDirectory()
+#     # print("Option in return back" + path)
+#     path = fl.goBackDirectory(path)
+#     cli(path)
--- files.py
+++ files.py
@@ -1,54 +1,48 @@
-from os import listdir, getcwd
-from os.path import isfile, join, splitext, isdir
+from os import listdir, getcwd, chdir
+from os.path import isfile, join, splitext, isdir, expanduser, dirname
 
 def isDirectory(path): # Check if a given file path is a file or a directory
-	if isdir(path):
-	    return True
-	else:
-		return False
+    if isdir(path):
+        return True
+    else:
+        return False
 
 def fileLabel(file, extension):
-	# icon = ""
-	# if extension == "folder":
-	# 	icon = u"\U0001F4C1"
-	# elif extension == ".png":
-	# 	icon = u"\U0001F5BC"
-	# elif extension == ".txt" or extension == ".text":
-	# 	icon = u"\U0001F5B9"
-	# elif extension == ".doc" or extension == ".docx" or extension == ".pdf":
-	# 	icon = u"\U0001F5BA"
-	# else:
-	# 	icon = u"\u003F"
+    label = f"{file}"
+    return label
 
-	label = f"{file}"
-	return label
-
-
+# find all files and folders in a given path
 def getAllFilesInGivenDirectory(path):
 	# files = [f for f in listdir(path) if isfile(join(path, f))]
-	dirs = listdir(path)
+    dirs = listdir(path)
 
-	files =[]
-	for file in dirs:
-		filePath = path + "/" + file
-		name, extension = splitext(filePath)
-		if extension == "" and isDirectory(filePath) : extension = "folder"
-		files.append({"name": file, "label": fileLabel(file, extension), "path": filePath, "type": extension})
-
-
-	return files
+    files =[]
+    for file in dirs:
+        filePath = path + "/" + file
+        name, extension = splitext(filePath)
+        if extension == "" and isDirectory(filePath) : extension = "dir"
+        files.append({"name": file, "label": fileLabel(file, extension), "path": filePath, "type": extension})
+        
+    return files
 
 
 
 def getCurrentDirectory():
-	currentDirectory = getcwd()
-	return currentDirectory
+    currentDirectory = getcwd()
+    return currentDirectory
+
+def getHomeDirectory(): # Users home address
+    return expanduser('~')
 
 def main():
-	#print(f"Your current directory: {getCurrentDirectory()}")
-	path = "/Users/aysilsimge/School/5. Dönem"
-	listFiles(getAllFilesInGivenDirectory(path))
+    #print(f"Your current directory: {getCurrentDirectory()}")
+    path = "/Users/aysilsimge/School/5. Dönem"
+    # print(getAllFilesInGivenDirectory(path))
+    #print(getHomeDirectory())
+    print(goBackDirectory(path))
 
+def goBackDirectory(path):
+    return dirname(path)
 
 if __name__ == "__main__":
     main()
