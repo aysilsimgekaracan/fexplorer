3d04519898fd452794ec7beb68e84b90e4707052
Ayşıl Simge Karacan <aysilsimge@gmail.com>
2022-02-16 21:46:34 +0300

    new release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'aysilsimgekaracan',                   # Type in your name
   author_email = 'aysilsimge@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/aysilsimgekaracan/fexplorer',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/aysilsimgekaracan/fexplorer/archive/refs/tags/v_0_1_0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/aysilsimgekaracan/fexplorer/archive/refs/tags/v_0_1_1.tar.gz',    # I explain this later on
   keywords = ['CLI', 'FILE EXPLORER', 'EXPLORER'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pick',
