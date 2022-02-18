b105401a618e65af666e542108d1081df6a04f57
Ayşıl Simge Karacan <aysilsimge@gmail.com>
2022-02-18 23:02:17 +0300

    remove unnecessary file 2

--- fexplorer/linux_commands_in_python.py
+++
@@ -1,8 +0,0 @@
-import os
-import subprocess
-
-os.system("ls -l")
-
-subprocess.call("cpu_util.sh")
-
-subprocess.run('while true; do CPU_USAGE=$(top -b -n2 -p 1 | fgrep "Cpu(s)"); DATE=$(date "+%Y-%m-%d %H:%M:"); echo $DATE - $CPU_USAGE >> cpu_usage; echo Recorded; sleep 5s; done', shell=True)
