1ee0a17cb7ff58c14b7dbecf1377b91919147dd6
Ayşıl Simge Karacan <aysilsimge@gmail.com>
2022-02-19 22:20:20 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
+with open('README.rst', encoding='utf-8') as f:
+  long_description = f.read()
+        
 setup(
   name = 'fexplorer',         # How you named your package folder (MyLib)
   packages = find_packages(include=['fexplorer', 'fexplorer.*']) ,   # Chose the same as "name"
   version = '0.0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Simple file explorer made with python',   # Give a short description about your library
+  long_description=long_description,
   author = 'aysilsimgekaracan',                   # Type in your name
   author_email = 'aysilsimge@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/aysilsimgekaracan/fexplorer',   # Provide either the link to your github or to your website
@@ -15,14 +19,15 @@ setup(
           'pick',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 2 - Pre-Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Environment :: Console',
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python',
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.9',
+    'Topic :: Desktop Environment :: File Managers'
   ],
   entry_points= {
         'console_scripts': ['fexplorer=fexplorer.__main__:main']
